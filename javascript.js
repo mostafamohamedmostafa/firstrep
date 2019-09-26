64f7e955aea4f781ebe973965041570fd21645f0
mostafamohamedmostafa <dev.mostafa.playstore@gmail.com>
2019-09-26 14:44:26 +0200

    create a var to connect first name and second name using java script

--- javascript.js
+++ javascript.js
@@ -9,3 +9,10 @@ var myfirstname = "Moustapha"
 
 //create second variable
 var mylastname = "Mohamed "
+
+// create variables of full name 
+/* create variable contains full name by join both of my first name var and my last name var 
+adding " " to make space betwwen first name and last name */
+// document .write an way to show full name variable
+var fullname = "hello my first name is " + "  " + myfirstname + "  " + mylastname
+document.write(fullname);
