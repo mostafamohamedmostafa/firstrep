ff3704956daad25f2ba3eb2d0250f8d041646c41
mostafamohamedmostafa <dev.mostafa.playstore@gmail.com>
2019-09-26 16:39:18 +0200

    adding varaibles stores numbers in java script

--- javascript.js
+++ javascript.js
@@ -16,3 +16,14 @@ adding " " to make space betwwen first name and last name */
 // document .write an way to show full name variable
 var fullname = "hello my first name is " + "  " + myfirstname + "  " + mylastname
 document.write(fullname);
+
+// create variables with numbers values
+// create var to join numbers info and show on html
+var myage = 29
+var myweight = 77
+var myheight = 1.77
+var numberofmobiles = 4
+var numberoffamilymembers = 30
+var numberfacts = "My age is "  + myage + " & " + "My weight is" + myweight + " & " + "My Height is " + myheight + " & " +" i have " + numberofmobiles + " Mobiles" 
++ " & " + " I have " + numberoffamilymembers + " Members"
+document.write('<br/>' , numberfacts);
