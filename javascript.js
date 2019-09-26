1adc905c415c1d461ea61c55f824778c177697d7
mostafamohamedmostafa <dev.mostafa.playstore@gmail.com>
2019-09-26 14:26:01 +0200

    make my first name variable in java script

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-alert("hello")
+// alert is the way to make pop up window with a certain message
+alert("welcome to java script basics")
+
+// create a variables
+// var keyword to declaration
+// myfirstname - name of variable 
+// "Moustapha " the value of myfirstname variable
+var myfirstname = "Moustapha"
